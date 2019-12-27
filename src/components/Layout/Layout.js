ed21c839b8a9a55b7afeb5e03e8831e74bd8a1c4
dsharonbgu <dsharon@post.bgu.ac.il>
2019-12-27 20:11:08 +0200

    create Layout and burgerbuilder

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,22 @@
+import  React from 'react';
+import Auxiliary from '../../hoc/Auxiliary';
+import classes from  './Layout.css';
+
+
+
+
+
+
+
+const layout = (props) => (
+    <Auxiliary>
+        <div>Toolbar, Sidebar, Backrop</div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Auxiliary>
+);
+
+
+
+export default layout;
